f1572c322ac064209382cabe22b046830b16d56d
Egor Alexandrovich <bbff33333@gmail.com>
2023-09-07 12:47:53 +0300

    07/09

---
+++ front/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+    return (
+        <div className="div">
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est voluptates nisi harum ea laborum debitis iusto repudiandae accusantium tempora porro corporis perferendis obcaecati aut, nulla rerum unde ut quibusdam dolor.
+            </p>
+        </div>
+    )
+
+}
+
+export default App;
